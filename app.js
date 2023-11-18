58cc7e8bd5b0c4ba6c385acab714f8649645cc44
ahmet keser <ahmetkeser006@gmail.com>
2023-11-19 00:26:45 +0300

    çeiri işlem karışıklığı giderildi

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const amountElement = document.getElementById("amount") //adet girişi
 const firstSelect = document.getElementById("firstCurrency") // türü
 const secondSelect = document.getElementById("secondCurrency") // coin
-const currency = new Currency("USDT","TRY")
+const currency = new Currency("USDT","DOGE")
+const ui= new UI(firstSelect,secondSelect)
 eventListeners()
 
 function eventListeners(){
@@ -10,16 +11,18 @@ function eventListeners(){
     amountElement.addEventListener("input",exchangeCurrency) // input değişimi izlenecek
     firstSelect.onchange = function(){ // ilk selec liste değeri takip edilecek
         currency.changeFirstCurrency(firstSelect.options[firstSelect.selectedIndex].textContent)
+        ui.changeFirst() 
     }
     secondSelect.onchange = function(){ // ikinci selec liste değeri takip edilecek
         currency.changeSecondCurrency(secondSelect.options[secondSelect.selectedIndex].textContent)
+        ui.changeSecond()
     }
     
 }
 function exchangeCurrency(){
     currency.chageAmount(amountElement.value)
     currency.exchange()
-    .then(result => console.log(result))
+    .then(result => ui.displayResult(result))
     .catch(err => console.log())
 }
 //https://api.binance.com/api/v3/avgPrice?symbol=USDTTRY
--- currency.js
+++ currency.js
@@ -6,14 +6,12 @@ class Currency{
         this.amount = null
     }
     exchange(){
-        let islem
-        if(this.secondCurrency === "TRY"){
-            islem=this.firstCurrency +this.secondCurrency
-        }else{
-            islem =this.secondCurrency + this.firstCurrency
-        }
+       
+        
+       
+        
         return new Promise((resolve,reject) =>{
-            fetch(this.url + islem )
+            fetch(this.url   + this.secondCurrency + this.firstCurrency)
         .then(response => response.json())
         .then(data => resolve(data.price * this.amount))// olumlu sonuçta girilen adetle güncel miktar çarpılarak geri dönüş verilir.
         .catch(err => reject("SEÇİLİ İŞLEM YOK" ,err)) // Olumsuz sonuç için geri dönüş yapılır.
@@ -25,14 +23,14 @@ class Currency{
     }
     chageAmount(amount){
         this.amount = amount // adet
-        console.log(amount)
+        console.log("---",amount)
     }
     changeFirstCurrency(newFirstCurrency){
         this.firstCurrency = newFirstCurrency
-        console.log("adet",this.firstCurrency)
+        console.log("üst",this.firstCurrency)
     }
-    changeSecondCurrency(newSecontCurrency){
-        this.secondCurrency = newSecontCurrency
-        console.log(this.secondCurrency)
+    changeSecondCurrency(newSecondCurrency){
+        this.secondCurrency = newSecondCurrency
+        console.log("alt",this.secondCurrency)
     }
 }
